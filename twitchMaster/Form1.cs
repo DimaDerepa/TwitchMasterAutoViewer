b4f93cc098faa746f12bad27a189fab725144618
DimaDerepa <dimaderepa1997@gmail.com>
2020-07-26 02:10:10 +0300

    Version 2.0 Choose and open promo translation and 1 from top block + check translation coef every 10 minutes and reopen if fell

--- twitchMaster/Form1.cs
+++ twitchMaster/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -11,23 +12,81 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Keys = OpenQA.Selenium.Keys;
+using System.Timers;
 
 namespace twitchMaster
 {
     public partial class Form1 : Form
     {
         static IWebDriver driver = new ChromeDriver(@"C:\Users\dimad\Desktop");
+        private static System.Timers.Timer aTimer;
         public Form1()
         {
             InitializeComponent();
+            
             Login();
-            Thread.Sleep(2000);
+         
+            pause(2000);
+           
             OpenPromo();
             OpenNewTab();
 
-            pause(7000);
+            pause(2000);
             OpenTop();
-    
+            SetTimer();
+
+        }
+        private void SetTimer()
+        {
+           
+            aTimer = new System.Timers.Timer(600000);
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            CheckTop();
+          
+            CheckPromo();
+           
+            
+        }
+        private void CheckPromo()
+        {
+            driver.SwitchTo().Window(driver.WindowHandles[0]);
+            if (driver.FindElement(By.XPath("/html/body/div[1]/div/div[4]/div[2]/div[3]/b")).Displayed)
+            {
+                return;
+            }
+            else
+            {
+                driver.Url = "https://twitchmaster.ru/";
+                pause(180000);
+                OpenPromo();
+            }
+
+        }
+        private void CheckTop()
+        {
+            driver.SwitchTo().Window(driver.WindowHandles[1]);
+            if (driver.FindElement(By.XPath("/html/body/div[1]/div/div[4]/div[2]/div[5]")).Displayed)
+            {
+                return;
+            }
+            else
+            {
+                driver.Url = "https://twitchmaster.ru/";
+                pause(180000);
+                OpenTop();
+            }
+
+        }
+        protected void WaitForPageLoad()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            wait.Until(driver => ((IJavaScriptExecutor)driver).ExecuteScript("return document.readyState").Equals("complete"));
         }
         public void OpenNewTab()
         {
@@ -37,15 +96,22 @@ namespace twitchMaster
         public void OpenPromo()
         {
             driver.Url = "https://twitchmaster.ru/";
+            pause(2000);
             driver.FindElement(By.PartialLinkText("Перейти к просмотру трансляции")).Click();
-            Thread.Sleep(5000);
+           
+            
+          
+
         }
         public void OpenTop()
         {
             driver.SwitchTo().Window(driver.WindowHandles[1]);
             driver.Url = "https://twitchmaster.ru/";
+            pause(2000);
             driver.FindElement(By.XPath("/html/body/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div/div[1]/a[2]/img")).Click();
-            Thread.Sleep(5000);
+        
+         
+      
         }
         public void pause(int vr)
         {
@@ -58,23 +124,26 @@ namespace twitchMaster
         }
         public void Login()
         {
-
             driver.Url = "https://twitchmaster.ru/";
-            pause(50000);
-
-            driver.FindElement(By.XPath("/html/body/div[1]/div[1]/div/div[2]/span[2]/a[2]")).Click();
+            pause(30000);
+            driver.Url = "https://twitchmaster.ru/static/about";
            
-            pause(20000);
-
+            WaitForPageLoad();
             driver.FindElement(By.XPath("/html/body/div[1]/div[1]/div/div[2]/span[2]/a[2]")).Click();
-
             pause(2000);
+
+
             driver.FindElement(By.Name("login")).SendKeys("GRO08Y");
             driver.FindElement(By.Name("mypw")).SendKeys("quakederepa");
-            Thread.Sleep(2000);
+            pause(2000);
             driver.FindElement(By.XPath("/html/body/div[4]/div[2]/form/div[5]/div")).Click();
         }
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
